2cc6bccc1aea6d5f21739077c658bf49418a184d
nicolas-ivanov <nickolas.ivanov@gmail.com>
2016-06-30 18:41:02 +0300

    added py requirements and setup lua torch files"

---
+++ setup_lua_torch.sh
@@ -0,0 +1,16 @@
+# install torch
+curl -s https://raw.githubusercontent.com/torch/ezinstall/master/install-deps | bash
+git clone https://github.com/torch/distro.git ~/torch --recursive
+cd ~/torch;
+./install.sh      # and enter "yes" at the end to modify your bashrc
+source ~/.bashrc
+
+# install cool packages
+luarocks install nngraph
+luarocks install optim
+luarocks install hdf5
+luarocks install nn
+
+# cool packages for nvidia gpus
+luarocks install cutorch
+luarocks install cunn
